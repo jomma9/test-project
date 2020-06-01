a01123c61c720be4aeb6f764ed6bfd1a159770ed
Stephen Jomma <40311397+jomma9@users.noreply.github.com>
2020-05-31 20:45:33 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styles.css";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return <div>Hello World!</div>;
+  }
+}
+
+export default App;
